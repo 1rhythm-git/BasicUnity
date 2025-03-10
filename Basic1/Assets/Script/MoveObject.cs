54317929f4d2a88916f77ba5ef779c6be21d429d
1rhythm-git <dmdckd@gmail.com>
2025-03-10 15:05:39 +0900

    Basic1

--- Basic1/Assets/Script/MoveObject.cs
+++ Basic1/Assets/Script/MoveObject.cs
@@ -8,8 +8,14 @@ public class MoveObject : MonoBehaviour
     
     void Update()
     {
-        //키 입력에 따라 이동
+        /*//키 입력에 따라 이동
         float move = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * move * speed * Time.deltaTime);
+        // 키보드 움직임 표현 = 방향 * 속도 * 타임델타타임(프레임보정)
+        transform.Translate(Vector3.right * move * speed * Time.deltaTime);*/
+                
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        /*transform.position += move * speed * Time.deltaTime;*/
+        transform.Translate(move * speed * Time.deltaTime);
+
     }
 }
