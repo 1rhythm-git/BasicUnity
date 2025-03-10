79e95535a51b03ab61fb3015c9a69fbd424057a1
1rhythm-git <dmdckd@gmail.com>
2025-03-10 10:45:44 +0900

    Basic1

---
+++ Basic1/Assets/Script/ConditionExample.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ConditionExample : MonoBehaviour
+{
+    //연산자와 조건문
+    public int health = 100; 
+      
+
+   
+    void Update()
+    {
+        health -= 1;
+        Debug.Log("Health:" + health);
+
+        if(health <=0)
+        {
+            Debug.Log("GameOver");
+        }
+    }
+}
